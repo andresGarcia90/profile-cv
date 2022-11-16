1532f7fc97b65d586cd63c3c36f5054add9193f0
andresGarcia90 <garcia.andi90@gmail.com>
2022-11-16 19:54:09 -0300

    adding scroll to

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { createRef, useRef } from "react";
 
 import "./App.css";
 import AboutPage from "./components/AboutPage/AboutPage";
@@ -8,13 +8,45 @@ import NavbarMain from "./components/NavbarMain/NavbarMain";
 import SkillsPage from "./components/SkillsPage/SkillsPage";
 
 function App() {
+
+  const mainRef = useRef();
+  const aboutRef = createRef();
+  const skillsRef = createRef();
+  const contactRef = createRef();
+
+
+  const handleClickNavbar = event => {
+    const key = event.target.text.toLowerCase();
+    console.log(key);
+
+    console.log(aboutRef);
+
+    switch (key) {
+      // case 'home':
+      //   mainRef.current.scrollIntoView();
+      //   break;
+      case 'about':
+        aboutRef.current.scrollIntoView();
+        break;
+        case 'skills':
+          skillsRef.current.scrollIntoView();
+        break;
+        case 'contact':
+          contactRef.current.scrollIntoView();
+        break;
+        
+      default:
+        break;
+    }
+  }
+
   return (
     <>
-      <NavbarMain></NavbarMain>
-      <MainPage></MainPage>
-      <AboutPage></AboutPage>
-      <SkillsPage></SkillsPage>
-      <ContactPage></ContactPage>
+      <NavbarMain clickNavbar={handleClickNavbar}></NavbarMain>
+      <MainPage innerRef={mainRef}></MainPage>
+      <AboutPage ref={aboutRef}></AboutPage>
+      <SkillsPage ref={skillsRef}></SkillsPage>
+      <ContactPage ref={contactRef}></ContactPage>
     </>
   );
 }
